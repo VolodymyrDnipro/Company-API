ba37945f0e19da79bd686d5c6289a08dabf5d94f
Vladimir <htreag@gmail.com>
2023-07-21 19:11:07 +0300

    BE-6_added_changes

--- services/auth.py
+++ services/auth.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Optional
 from sqlalchemy.ext.asyncio import AsyncSession
 from managers.user_manager import CRUDBase
 from db.models.users import User
@@ -6,16 +6,17 @@ from utils.hashing import Hasher
 
 
 class AuthService:
-    async def authenticate_user(self, email: str, password: str, session: AsyncSession) -> Union[User, None]:
-        user_crud = CRUDBase[User](User, session)
-        user = await user_crud.get_by_field(email, "email")
+    def __init__(self, session: AsyncSession):
+        self.user_crud = CRUDBase[User](User, session)
+
+    async def authenticate_user(self, email: str, password: str) -> Optional[User]:
+        user = await self.user_crud.get_by_field(email, "email")
         if user is None:
             return
         if not Hasher.verify_password(password, user.hashed_password):
             return
         return user
 
-    async def get_user_by_email_for_auth(self, email: str, session: AsyncSession) -> User:
-        user_crud = CRUDBase[User](User, session)
-        user = await user_crud.get_by_field(email, "email")
+    async def get_user_by_email_for_auth(self, email: str) -> User:
+        user = await self.user_crud.get_by_field(email, "email")
         return user
--- services/users.py
+++ services/users.py
@@ -8,36 +8,34 @@ from schemas.users import UserCreate, UpdateUserRequest
 
 
 class UsersService:
-    async def get_all_users(self, session: AsyncSession) -> List[User]:
-        user_crud = CRUDBase[User](User, session)
-        return await user_crud.get_all()
+    def __init__(self, session: AsyncSession):
+        self.user_crud = CRUDBase[User](User, session)
 
-    async def get_user_by_id(self, user_id: int, session: AsyncSession) -> User:
-        user_crud = CRUDBase[User](User, session)
-        user = await user_crud.get_by_field(user_id, field_name='user_id')
+    async def get_all_users(self) -> List[User]:
+        return await self.user_crud.get_all()
+
+    async def get_user_by_id(self, user_id: int) -> User:
+        user = await self.user_crud.get_by_field(user_id, field_name='user_id')
         return user
 
-    async def create_user(self, user_data: UserCreate, session: AsyncSession) -> User:
-        user_crud = CRUDBase[User](User, session)
+    async def create_user(self, user_data: UserCreate) -> User:
         hashed_password = Hasher.get_password_hash(user_data.password)
         user = User(name=user_data.name, surname=user_data.surname, email=user_data.email,
                     hashed_password=hashed_password)
-        created_user = await user_crud.create(user)
+        created_user = await self.user_crud.create(user)
         return created_user
 
-    async def update_user(self, user_id: int, user_data: UpdateUserRequest, session: AsyncSession) -> User:
-        user_crud = CRUDBase[User](User, session)
-        existing_user = await user_crud.get_by_field(user_id, field_name='user_id')
+    async def update_user(self, user_id: int, user_data: UpdateUserRequest) -> User:
+        existing_user = await self.user_crud.get_by_field(user_id, field_name='user_id')
         if existing_user is None:
             raise HTTPException(status_code=404, detail="User not found")
 
-        updated_user = await user_crud.update(existing_user, user_data.dict())
+        updated_user = await self.user_crud.update(existing_user, user_data.dict())
         return updated_user
 
-    async def delete_user(self, user_id: int, session: AsyncSession) -> None:
-        user_crud = CRUDBase[User](User, session)
-        existing_user = await user_crud.get_by_field(user_id, field_name='user_id')
+    async def delete_user(self, user_id: int) -> None:
+        existing_user = await self.user_crud.get_by_field(user_id, field_name='user_id')
         if existing_user is None:
             raise HTTPException(status_code=404, detail="User not found")
 
-        await user_crud.delete(existing_user)
+        await self.user_crud.delete(existing_user)
