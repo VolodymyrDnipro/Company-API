74bb49b6bd5a4153094d97315d66e5f1bb16218d
Volodymyr <htreag@gmail.com>
2023-08-03 08:38:15 +0300

    BE-12_added_changes

--- services/quizzes.py
+++ services/quizzes.py
@@ -96,8 +96,6 @@ class QuizService:
             # Insert the Question object to the database to generate the question_id
             await self.question_crud.create(question)
 
-            # created_answers = []
-
             for answer_data in question_data.answers:
                 answer = Answer(
                     quiz_id=created_quiz.quiz_id,
@@ -105,14 +103,7 @@ class QuizService:
                     answer_text=answer_data.answer_text,
                     is_correct=answer_data.is_correct
                 )
-
-                # Insert the Answer object to the database to generate the answer_id
                 await self.answers_crud.create(answer)
-
-                # created_answers.append(answer)
-
-            # question.answers = created_answers
-            # created_quiz.questions.append(question)
 
         return created_quiz
 
@@ -150,42 +141,33 @@ class QuizService:
         return updated_answer
 
     async def create_user_answer(self, user_email: str, user_answer_data: UserAnswersCreate) -> UserAnswers:
-        # Знайти користувача за email
         auth_user = await self.find_auth_user_by_email(user_email)
         if not auth_user:
             raise HTTPException(status_code=404, detail="No user found")
 
         quiz = await self.get_quiz_by_quiz_id(user_answer_data.quiz_id)
 
-        # Знайти усі питання, які є у квізі за його quiz_id
         questions = await self.get_all_questions(user_answer_data.quiz_id)
         if not questions:
             raise HTTPException(status_code=404, detail="No quiz found")
 
-        # Отримати всі відповіді користувача за user_id
         all_user_answers = await self.user_answer_crud.get_all(user_id=auth_user.user_id)
 
-        # Відфільтрувати відповіді, щоб залишити ті, що належать питанням зі списку questions
         user_answers = [answer for answer in all_user_answers if
                         answer.question_id in [q.question_id for q in questions]]
 
-        # Перевірити, чи користувач вже відповідав на питання user_answer_data.question_id
         if any(answer.question_id == user_answer_data.question_id for answer in user_answers):
 
-            # Перевірити, що кількість знайдених відповідей дорівнює кількості питань у квізі
             if len(user_answers) == len(questions):
 
-                # Знайти останню відповідь користувача на будь-яке питання у цьому квізі за допомогою timestamp
                 last_answer = max(user_answers, key=lambda x: x.timestamp)
 
-                # Перевірити, чи пройшло frequency_in_days з моменту останньої відповіді до поточного часу
                 if datetime.utcnow() - last_answer.timestamp < timedelta(days=quiz.frequency_in_days):
                     raise HTTPException(status_code=400,
                                         detail=f"You can start a new quiz process only after completing it {quiz.frequency_in_days} days")
             else:
                 raise HTTPException(status_code=400, detail="This question has already been answered before")
 
-        # Створити нову відповідь користувача в таблиці UserAnswers
         created_user_answer = await self.user_answer_crud.create(UserAnswers(
             user_id=auth_user.user_id,
             quiz_id=user_answer_data.quiz_id,
@@ -203,33 +185,21 @@ class QuizService:
 
         quiz_results = []
 
-        # # 1. Використовуючи user_id знаходимо у моделі UserAnswers усі quiz_id використовуємо get_all,
-        # user_answers = await self.user_answer_crud.get_all(user_id=user.user_id)
-        # # знаходимо усі обьєкти по юзеру. їх 2 штуки і списку
-        #
-        # for user_answer in user_answers: #1
-        # 2. У циклі Беремо перший quiz_id  і по ньому знаходимо усі question_id
         questions = await self.user_answer_crud.get_all(user_id=user.user_id)
 
         for question in questions:
-        # 3. У циклі Далі за першим question_id фільтруємо із параметром user_answer_id усі answer_id
             answers = await self.user_answer_crud.get_all(question_id=question.question_id)
 
             for answer in answers:
-                # 4. Далі  беремо перший quiz_id що знайшли у таблиці UserAnswers і знаходимо його у таблиці Answer.
                 quiz_data = await self.quizzes_crud.get_by_field(answer.quiz_id, field_name='quiz_id')
 
-                # 5. Далі беремо перший question_id що знайшли у таблиці UserAnswers і знаходимо його у таблиці Answer.
                 question_data = await self.question_crud.get_by_field(question.question_id,
                                                                       field_name='question_id')
 
-                # 6. Далі беремо перший answer_id що знайшли у таблиці UserAnswers і знаходимо його у таблиці Answer.
                 answer_data = await self.answers_crud.get_by_field(answer.answer_id, field_name='answer_id')
 
-                # 7. Далі дивимось на поле is_correct і що саме там стоїть  False чи True і переносимо в модель QuizResult в поле result.
                 result = answer_data.is_correct
 
-                # І далі заповнюємо усі інші поля в моделі QuizResult:
                 quiz_result = QuizResult(
                     user_id=user.user_id,
                     quiz_id=quiz_data.quiz_id,
@@ -237,7 +207,6 @@ class QuizService:
                     user_answer_id=answer.user_answer_id,
                     result=result
                 )
-                # await self.quiz_result_crud.create(quiz_result)
                 quiz_results.append(quiz_result)
         for quiz_result in quiz_results:
             await self.quiz_result_crud.create(quiz_result)
@@ -245,13 +214,10 @@ class QuizService:
         return quiz_results
 
     async def calculate_and_update_average_score(self, user_id: int) -> int:
-        # Step 2: Get all QuizResult objects for the given user_id
         quiz_results = await self.quiz_result_crud.get_all(user_id=user_id)
 
-        # Step 3: Calculate the number of QuizResult objects with result == True
         true_count = sum(1 for quiz_result in quiz_results if quiz_result.result)
 
-        # Step 4: Update the average_score in the Users table
         user = await self.find_user_by_user_id(user_id)
         if not user:
             raise HTTPException(status_code=404, detail="No user found")
