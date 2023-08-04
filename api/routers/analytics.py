d583ea0158d4fffa0c3f5c9e1a8309d62721c377
Volodymyr <htreag@gmail.com>
2023-08-04 17:54:54 +0300

    BE-15_completed

--- api/routers/analytics.py
+++ api/routers/analytics.py
@@ -1,30 +1,112 @@
-from fastapi import APIRouter, Depends, HTTPException, Body, Path, Response
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, Body, Path, Response, Query
 from fastapi_pagination import Page, Params, paginate
 from fastapi.responses import FileResponse
+from datetime import date
 
-from schemas.quizzes import *
-from schemas.questions import *
-from schemas.answers import *
+from schemas.questions import QuestionUpdate
+from schemas.answers import AnswerUpdate
 from services.company import CompanyService
+from services.users import UsersService
 from services.quizzes import QuizService
 from services.analytics import AnalyticsService
-from utils.dependencies import get_company_service, get_quiz_service, get_analytics_service
+from utils.dependencies import get_company_service, get_quiz_service, get_analytics_service, get_users_service
 from api.routers.users import get_user_data
 
 analytics_router = APIRouter()
 
 
-# Route to create a new quiz
-@analytics_router.get("/analytics/rating", response_model=UserRatingResponse)
-async def get_user_rating(
-        user_id: int,
+@analytics_router.get("/analytics/rating/", response_model=int)
+async def user_gets_average_rating_about_himself(
         user_email: str = Depends(get_user_data),
-        quiz_service: QuizService = Depends(get_quiz_service),
+        analytics_service: AnalyticsService = Depends(get_analytics_service),
+):
+    try:
+        user_rating = await analytics_service.get_self_rating(user_email)
+    except HTTPException as exc:
+        raise exc
+    return user_rating
+
+
+@analytics_router.get("/analytics/rating/quizzes", response_model=List[dict])
+async def get_user_rating_by_all_quizzes_by_date(
+        start_date: date = Query(...),
+        end_date: date = Query(...),
+        user_email: str = Depends(get_user_data),
+        analytics_service: AnalyticsService = Depends(get_analytics_service),
+):
+    try:
+        user_rating = await analytics_service.get_user_rating_by_all_quizzes_by_date(user_email, start_date, end_date)
+    except HTTPException as exc:
+        raise exc
+    return user_rating
+
+
+@analytics_router.get("/analytics/quizzes/last_completed", response_model=List[dict])
+async def get_quizzes_and_last_completion_time(
+        user_email: str = Depends(get_user_data),
+        analytics_service: AnalyticsService = Depends(get_analytics_service),
+):
+    try:
+        quiz_completion_times = await analytics_service.get_quizzes_and_last_completion_time(user_email)
+    except HTTPException as exc:
+        raise exc
+    return quiz_completion_times
+
+
+# # # Company Analytics # # #
+@analytics_router.get("/analytics/rating/company/{company_id}", response_model=List[dict])
+async def company_get_average_scores(
+        company_id: int = Path(),
+        start_date: date = Query(...),
+        end_date: date = Query(...),
+        user_email: str = Depends(get_user_data),
         company_service: CompanyService = Depends(get_company_service),
         analytics_service: AnalyticsService = Depends(get_analytics_service),
 ):
     try:
-        user_rating = await user_service.get_user_rating(user_id)
+        # Check if the user is the owner or admin of the company
+        await company_service.check_who_this_user_in_company_admin_or_owner_by_company_id(user_email, company_id)
+
+        user_rating = await analytics_service.company_get_average_scores(company_id, start_date, end_date)
     except HTTPException as exc:
         raise exc
     return user_rating
+
+
+@analytics_router.get("/analytics/rating/company/{company_id}/user/{user_id}/quizzes", response_model=List[dict])
+async def company_get_average_scores_by_all_users(
+        company_id: int = Path(),
+        user_id: int = Path(),
+        start_date: date = Query(...),
+        end_date: date = Query(...),
+        user_email: str = Depends(get_user_data),
+        company_service: CompanyService = Depends(get_company_service),
+        analytics_service: AnalyticsService = Depends(get_analytics_service),
+):
+    try:
+        # Check if the user is the owner or admin of the company
+        await company_service.check_who_this_user_in_company_admin_or_owner_by_company_id(user_email, company_id)
+
+        user_rating = await analytics_service.company_get_average_scores_by_all_users(user_id, company_id, start_date, end_date)
+    except HTTPException as exc:
+        raise exc
+    return user_rating
+
+
+@analytics_router.get("/analytics/rating/company/{company_id}/users", response_model=List[dict])
+async def company_get_all_users_and_last_completion_time(
+        company_id: int = Path(),
+        user_email: str = Depends(get_user_data),
+        company_service: CompanyService = Depends(get_company_service),
+        analytics_service: AnalyticsService = Depends(get_analytics_service),
+):
+    try:
+        # Check if the user is the owner or admin of the company
+        await company_service.check_who_this_user_in_company_admin_or_owner_by_company_id(user_email, company_id)
+
+        users_completion_times = await analytics_service.company_get_all_users_and_last_completion_time(company_id)
+    except HTTPException as exc:
+        raise exc
+    return users_completion_times
--- services/analytics.py
+++ services/analytics.py
@@ -1,6 +1,7 @@
 from typing import Union, List
 
 from fastapi import HTTPException, status
+from datetime import date
 import json
 from datetime import datetime, timedelta
 import asyncio_redis
@@ -26,3 +27,120 @@ class AnalyticsService:
         self.question_crud = CRUDBase[Question](Question, session)
         self.answers_crud = CRUDBase[Answer](Answer, session)
         self.user_answer_crud = CRUDBase[UserAnswers](UserAnswers, session)
+
+    async def find_auth_user_by_email(self, user_email: str) -> User:
+        return await self.user_crud.get_by_field(user_email, field_name='email')
+
+    async def find_user_by_user_id(self, user_id: int) -> User:
+        return await self.user_crud.get_by_field(user_id, field_name='user_id')
+
+    async def find_company_by_company_id(self, company_id: int) -> Company:
+        return await self.company_crud.get_by_field(company_id, field_name='company_id')
+
+    async def find_all_company_members_by_company_id(self, company_id: int) -> List[CompanyMembership]:
+        return await self.membership_crud.get_all(company_id=company_id)
+
+    async def find_user_in_company(self, company_id: int, user_id: int) -> CompanyMembership:
+        return await self.membership_crud.get_by_fields(company_id=company_id, user_id=user_id)
+
+    async def find_user_result_by_user_id(self, user_id: int) -> List[QuizResult]:
+        return await self.quiz_result_crud.get_all(user_id=user_id)
+
+    async def find_user_results_by_user_id_and_date(self, user_id: int, start_date: date, end_date: date) -> List[QuizResult]:
+        return await self.quiz_result_crud.get_all(user_id=user_id, timestamp__ge=start_date, timestamp__le=end_date)
+
+    async def get_self_rating(self, user_email: str) -> int:
+        user = await self.find_auth_user_by_email(user_email)
+        user_rating = user.average_score
+        return user_rating
+
+    async def get_user_rating_by_all_quizzes_by_date(self, user_email: str, start_date: date, end_date: date) -> List[dict]:
+        user = await self.find_auth_user_by_email(user_email)
+        if not user:
+            raise HTTPException(status_code=404, detail="No user found")
+
+        quiz_results = await self.find_user_results_by_user_id_and_date(user.user_id, start_date, end_date)
+        if not quiz_results:
+            raise HTTPException(status_code=404, detail="No quiz found")
+
+        average_scores = []
+
+        for quiz_result in quiz_results:
+            quiz_id = quiz_result.quiz_id
+            true_count = sum(1 for result in quiz_results if result.quiz_id == quiz_id and result.result)
+            average_scores.append({"quiz": quiz_id, "average_count": true_count})
+
+        return average_scores
+
+    async def get_quizzes_and_last_completion_time(self, user_email: str) -> List[dict]:
+        user = await self.find_auth_user_by_email(user_email)
+        quizzes = await self.find_user_result_by_user_id(user_id=user.user_id)
+        quiz_completion_times = {}
+
+        for quiz in quizzes:
+            quiz_id = quiz.quiz_id
+            timestamp = quiz.timestamp
+            if quiz_id in quiz_completion_times:
+                quiz_completion_times[quiz_id] = max(quiz_completion_times[quiz_id], timestamp)
+            else:
+                quiz_completion_times[quiz_id] = timestamp
+
+        result = [{"quiz_id": key, "last_completion_time": value} for key, value in quiz_completion_times.items()]
+        return result
+
+    async def company_get_average_scores(self, company_id: int, start_date: date, end_date: date) -> List[dict]:
+        company_members = await self.find_all_company_members_by_company_id(company_id)
+        average_scores = []
+
+        for member in company_members:
+            user_id = member.user_id
+            quiz_results = await self.find_user_results_by_user_id_and_date(user_id, start_date, end_date)
+
+            if not quiz_results:
+                # No quiz found for the user, skip to the next member
+                continue
+
+            true_count = sum(1 for quiz_result in quiz_results if quiz_result.result)
+
+            average_scores.append({"user": user_id, "average_count": true_count})
+
+        return average_scores
+
+
+    async def company_get_average_scores_by_all_users(self, user_id: int, company_id: int, start_date: date, end_date: date) -> List[dict]:
+        user = await self.find_user_by_user_id(user_id)
+        if not user:
+            raise HTTPException(status_code=404, detail="No user found")
+
+        # check user in company
+        company_member = await self.find_user_in_company(company_id, user_id)
+        if not company_member:
+            raise HTTPException(status_code=404, detail="No user found in company")
+
+        quiz_results = await self.find_user_results_by_user_id_and_date(user_id, start_date, end_date)
+
+        count_result_true = []
+
+        result_true_count = sum(1 for quiz_result in quiz_results if quiz_result.result)
+
+        count_result_true.append({"user": user_id, "average_count": result_true_count})
+
+        return count_result_true
+
+    async def company_get_all_users_and_last_completion_time(self, company_id: int) -> List[dict]:
+        company_members = await self.find_all_company_members_by_company_id(company_id)
+
+        user_last_completion_times = []
+
+        for member in company_members:
+            user_id = member.user_id
+            last_completion_date = await self.quiz_result_crud.get_by_field(user_id, field_name="user_id")
+            if last_completion_date:
+                last_completion_date_str = last_completion_date.strftime("%Y-%m-%d %H:%M:%S")
+                user_last_completion_times.append({"user": user_id, "last_date": last_completion_date_str})
+
+        return user_last_completion_times
+
+
+
+
